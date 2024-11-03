36a91500ce590678ab76144425ed86dc54c86054
dilrun2001 <dilrun2001@gmail.com>
2024-11-03 15:30:03 +0900

    db연결 홈이랑 로그인 바꾸고 각리스트에 버튼추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'pages/cpu_page.dart';
+import 'pages/explanation.dart';
+import 'pages/graphics_page.dart';
 import 'pages/login_page.dart';
 import 'pages/home_page.dart';
+import 'pages/mainboard_page.dart';
 import 'pages/signup_page.dart';
-import 'pages/reset_password_page.dart'; // 비밀번호 재설정 페이지 import
+import 'pages/reset_password_page.dart';
+import 'pages/warning_page.dart'; // 비밀번호 재설정 페이지 import
 
 void main() {
   runApp(MyApp());
@@ -15,12 +20,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
+      initialRoute: '/home',
       routes: {
         '/': (context) => LoginPage(),
         '/home': (context) => HomePage(),
         '/signup': (context) => SignupPage(),
         '/reset_password': (context) => ResetPasswordPage(), // 비밀번호 재설정 경로 추가
+        '/cpu': (context) => CpuPage(),
+        '/graphics': (context) =>GraphicsPage(),
+        '/mainboard': (context) =>MainboardPage(),
+        '/warning': (context) =>WarningPage(),
+        '/explanation': (context) =>ExplanationPage(),
       },
     );
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,39 +1,214 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    // 음식 사진 데이터
+    List<Map<String, dynamic>> dataList = [
+      {
+        "category": "cpu",
+        "imgUrl": "./assets/images/cpu.png",
+        "goWhere": "/cpu",
+      },
+      {
+        "category": "그래픽카드",
+        "imgUrl": "./assets/images/graphics.jpg",
+        "goWhere": "/graphics",
+      },
+      {
+        "category": "메인보드",
+        "imgUrl": "./assets/images/mainboard.jpg",
+        "goWhere": "/mainboard",
+      },
+      {
+        "category": "조립시 주의사항",
+        "imgUrl": "./assets/images/warning.png",
+        "goWhere": "/warning",
+      },
+      {
+        "category": "부품 설명",
+        "imgUrl": "./assets/images/explanation.png",
+        "goWhere": "/explanation",
+      },
+    ];
+
+    // 화면에 보이는 영역
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        title: Text("컴퓨터 부품 앱"),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.logout),
-            onPressed: () {
-              // 로그아웃 버튼을 누르면 로그인 페이지로 이동하고, 홈 페이지는 스택에서 제거
-              Navigator.pushReplacementNamed(context, '/');
-            },
+        elevation: 0, // 그림자 없애기
+        backgroundColor: Colors.white, // 배경 색상
+        centerTitle: false, // title 중앙 정렬
+        iconTheme: IconThemeData(color: Colors.black), // app bar icon color
+        title: Text(
+          "컴알못 도우미",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          // 검색
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "상품을 검색해주세요.",
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black),
+                ),
+                // 돋보기 아이콘
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () {
+                    print("돋보기 아이콘 클릭");
+                  },
+                ),
+              ),
+            ),
+          ),
+          Divider(height: 1),
+          // 카테고리 목록
+          Expanded(
+            child: ListView.builder(
+              itemCount: dataList.length,
+              itemBuilder: (context, index) {
+                Map<String, dynamic> data = dataList[index];
+                String category = data["category"];
+                String imgUrl = data["imgUrl"];
+                String goWhere = data["goWhere"];
+
+                return Card(
+                  margin: const EdgeInsets.all(8),
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Image.asset(
+                        imgUrl,
+                        width: double.infinity,
+                        height: 120,
+                        fit: BoxFit.cover,
+                      ),
+                      Container(
+                        width: double.infinity,
+                        height: 120,
+                        color: Colors.black.withOpacity(0.5),
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        height: 120,
+                        child: TextButton(
+                            onPressed: () {
+                              Navigator.pushReplacementNamed(context, goWhere);
+                            },
+                            child: Text(
+                              category,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 36,
+                              ),
+                            ),
+                        ),
+                      ),
+
+                    ],
+                  ),
+                );
+              },
+            ),
           ),
         ],
       ),
-      body: Center(
+      drawer: Drawer(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('환영합니다!'),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/categories'); // 부품 카테고리 페이지로 이동
-              },
-              child: Text('부품 카테고리'),
+            DrawerHeader(
+              margin: const EdgeInsets.all(0),
+              decoration: BoxDecoration(
+                color: Colors.amber,
+              ),
+              child: SizedBox(
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 36,
+                      backgroundColor: Colors.white,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Image.network(
+                          "https://i.ibb.co/CwzHq4z/trans-logo-512.png",
+                          width: 62,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 13),
+                    Text(
+                      "닉네임",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "hello@world.com",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/cart'); // 장바구니 페이지로 이동
+            AspectRatio(
+              aspectRatio: 12 / 4,
+              child: PageView(
+                children: [
+                  Image.network(
+                    "https://i.ibb.co/Q97cmkg/sale-event-banner1.jpg",
+                  ),
+                  Image.network(
+                    "https://i.ibb.co/GV78j68/sale-event-banner2.jpg",
+                  ),
+                  Image.network(
+                    "https://i.ibb.co/R3P3RHw/sale-event-banner3.jpg",
+                  ),
+                  Image.network(
+                    "https://i.ibb.co/LRb1VYs/sale-event-banner4.jpg",
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              title: Text(
+                '구매내역',
+                style: TextStyle(fontSize: 18),
+              ),
+              trailing: Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.black,
+              ),
+              onTap: () {
+                Navigator.pop(context);
               },
-              child: Text('장바구니'),
+            ),
+            ListTile(
+              title: Text(
+                '저장한 레시피',
+                style: TextStyle(fontSize: 18),
+              ),
+              trailing: Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.black,
+              ),
+              onTap: () {
+                Navigator.pop(context);
+              },
             ),
           ],
         ),
